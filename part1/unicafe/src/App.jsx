93cb3378f6f84d671ea2926b8b9d0f0f0104500f
Eduardo Função <eduardofuncao@hotmail.com>
2024-07-05 12:08:28 -0300

    finish exercises 1-12 to 1-14 and reorganize project structure

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -35,6 +35,7 @@ const Button = ({ onClick, text }) => {
 
 const App = () => {
   // save clicks of each button to its own state
+  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -52,13 +53,13 @@ const App = () => {
 
   const handleBad = () => {
     setBad(bad+1)
+    debugger
     setTotal(total+1)
   }
 
   return (
     <div>
       <h1>give feedback</h1>
-
       <Button onClick={handleGood} text='good' /> 
       <Button onClick={handleNeutral} text='neutral' /> 
       <Button onClick={handleBad} text='bad' /> 
